f60772d30a6b32ffb71c497e8184e615b76fe6fc
LINKER <temp2temp@daum.net>
2017-07-30 18:20:55 +0900

    Add files via upload

---
+++ 한글공모전/index.php
@@ -0,0 +1,63 @@
+﻿<!DOCTYPE html>
+<head>
+<style>
+body {
+background: url('q1.png') no-repeat center center fixed;
+-moz-background-size: cover;
+-webkit-background-size: cover;
+-o-background-size: cover;
+background-size: cover;
+}
+</style>
+    <meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, user-scalable=no">
+    <title>시나브로 사전</title>
+</head>
+<center>
+    <body background="q1.png">
+        <b><font size="7" face="문체부 훈민정음체">시나브로 사전</b></font>
+<table border=2px bgcolor="#e4e38b">
+<tr>
+ <td colspan="2"width="350" align="center"><font size="5" face="문체부 훈민정음체"color="black">질의응답</td>
+</tr>
+<tr align="center" height="50">
+ <td><font color="black">질1. 영어인 단어만<br>번역 되나요?</font></td>
+ <td><font color="black">답1. 영어, 중어, 일어, 한문, <br>신조어등이 있습니다.</font>
+</td>
+
+<tr align="center"height="50">
+ <td><font color="black">질2. 사전의 범위는<br>어디까지 인가요?</font></td>
+ <td><font color="black">답2. 현재는 국립국어원에<br>등록된 정보들만 있습니다.<br>추후, 정보의 양을 늘릴<br>예정입니다.</font></td>
+ 
+ <tr align="center"height="50">
+ <td><font color="black">질3. 검색은 영어로<br>해야되나요?</font></td>
+ <td><font color="black">답3. 정립된 한글로 하시고,<br>띄어쓰기를 꼭 해야 합니다.<br>예)CCTV Ⅹ 시시티브이 ○<br>사이버대학 Ⅹ 사이버 대학 ○</font></td>
+ 
+ <tr align="center"height="50">
+ <td><font color="black">질4. 단어가 기억이<br>안나요.</font></td>
+ <td><font color="black">답4. 단어 한 글자라도<br>입력시 비슷한 단어가<br>제공됩니다.<br>
+예)콘 -> 이모티콘, 콘텐츠...</font>
+</td>
+
+ <tr align="center"height="50">
+ <td><font color="black">질5. 입력을 하지 않으면<br>어떻게 되나요?</font></td>
+ <td><font color="black">답5. 서버에 저장된 모든<br>정보가 출력됩니다.</font></td>
+</tr> 
+</table>
+<br>
+<form action="search.php" method="get">
+<li><label><font size="4"face="문체부 훈민정음체">변환 할 단어 :</font><input type="text" name="change"/></label></li>
+<br>
+<input type="submit" value="검색하기"></li>
+</pre></form></center>
+<footer>
+<pre>
+
+<font size="1"><b>Copyright ⓒ 시나브로</b>
+<b>기획/서버 : 이찬우(Zeropen) 
+안드로이드 : 강성준(AnA), 김민규(RG)
+UI/일러스트 : 오정식(App:pie)</b></font></pre>
+</footer>
+<href style="display: none"><a href="quiz.html">문제</a></style>
+</body>
+</html>
